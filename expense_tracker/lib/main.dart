020615a01dea67df9090ec9bcaf9890d773494b2
Paul Ruiz <ptruiz@google.com>
2023-10-12 12:22:41 -0600

    playing with themeing and color schemes

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181)
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        //scaffoldBackgroundColor: Color.fromARGB(255, 225, 149, 255)
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: Expenses(),
       debugShowCheckedModeBanner: false,
     ),
