63b1e8b08938dc97063df5e0489da08bf97f7385
Paul Ruiz <ptruiz@google.com>
2023-10-12 13:09:30 -0600

    learned how to support dark mode

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -5,9 +5,32 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+  brightness: Brightness.dark
+);
+
 void main() {
   runApp(
     MaterialApp(
+      themeMode: ThemeMode.system,
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardTheme().copyWith(
+          elevation: 16,
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            elevation: 8,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer
+          )
+        ),
+      ),
+      
       theme: ThemeData().copyWith(
         useMaterial3: true,
         //scaffoldBackgroundColor: Color.fromARGB(255, 225, 149, 255)
