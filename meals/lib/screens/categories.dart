b50f7f4d97ed991746e9874492c80125b0ef2457
Paul Ruiz <ptruiz@google.com>
2023-10-29 21:29:09 -0600

    passing in dynamic info for routing in list->detail setup

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(builder: (ctx) => MealsScreen(
-          meals: [],
-          title: "some title"
+          meals: filteredMeals,
+          title: category.title
         )
       )
     );
@@ -33,7 +37,7 @@ class CategoriesScreen extends StatelessWidget {
           // alternative: availableCategories.map((category) => CategoryGridItem(category: category)).toList()
           for (final category in availableCategories)
             CategoryGridItem(category: category, onSelectCategory: () {
-              _selectCategory(context);
+              _selectCategory(context, category);
             },)
         ],
       ),
