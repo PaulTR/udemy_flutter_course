23a0384991053a7a3cbecbffd286281e037e8314
Paul Ruiz <ptruiz@google.com>
2024-01-01 05:54:34 -0700

    finished tutorial for tinder clone

--- tinder_clone/lib/CardsStackWidget.dart
+++ tinder_clone/lib/CardsStackWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tinder_clone/ActionButtonWidget.dart';
 import 'package:tinder_clone/DragWidget.dart';
 import 'package:tinder_clone/Profile.dart';
 
@@ -9,31 +10,40 @@ class CardsStackWidget extends StatefulWidget {
   State<CardsStackWidget> createState() => _CardsStackWidgetState();
 }
 
-class _CardsStackWidgetState extends State<CardsStackWidget> {
-  List<Profile> dragabbleItems = [
+class _CardsStackWidgetState extends State<CardsStackWidget>
+    with SingleTickerProviderStateMixin {
+  List<Profile> draggableItems = [
     const Profile(
-        name: 'Sexy Beast',
-        distance: '10 miles away',
-        imageAsset: 'assets/me.jpeg'),
+        name: 'Paul', distance: '10 miles away', imageAsset: 'assets/me.jpeg'),
     const Profile(
-        name: 'Sexy Beast',
-        distance: '10 miles away',
-        imageAsset: 'assets/me.jpeg'),
+        name: 'Paul', distance: '10 miles away', imageAsset: 'assets/me.jpeg'),
     const Profile(
-        name: 'Sexy Beast',
-        distance: '10 miles away',
-        imageAsset: 'assets/me.jpeg'),
+        name: 'Paul', distance: '10 miles away', imageAsset: 'assets/me.jpeg'),
     const Profile(
-        name: 'Sexy Beast',
-        distance: '10 miles away',
-        imageAsset: 'assets/me.jpeg'),
+        name: 'Paul', distance: '10 miles away', imageAsset: 'assets/me.jpeg'),
     const Profile(
-        name: 'Sexy Beast',
-        distance: '10 miles away',
-        imageAsset: 'assets/me.jpeg'),
+        name: 'Paul', distance: '10 miles away', imageAsset: 'assets/me.jpeg'),
   ];
 
   ValueNotifier<Swipe> swipeNotifier = ValueNotifier(Swipe.none);
+  late final AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        draggableItems.removeLast();
+        _animationController.reset();
+
+        swipeNotifier.value = Swipe.none;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,13 +57,93 @@ class _CardsStackWidgetState extends State<CardsStackWidget> {
             builder: (context, swipe, _) => Stack(
               clipBehavior: Clip.none,
               alignment: Alignment.center,
-              children: List.generate(dragabbleItems.length, (index) {
-                return DragWidget(
-                  profile: dragabbleItems[index],
-                  index: index,
-                  swipeNotifier: swipeNotifier,
-                );
+              children: List.generate(draggableItems.length, (index) {
+                if (index == draggableItems.length - 1) {
+                  return PositionedTransition(
+                    rect: RelativeRectTween(
+                      begin: RelativeRect.fromSize(
+                          const Rect.fromLTWH(0, 0, 580, 340),
+                          const Size(580, 340)),
+                      end: RelativeRect.fromSize(
+                          Rect.fromLTWH(
+                              swipe != Swipe.none
+                                  ? swipe == Swipe.left
+                                      ? -300
+                                      : 300
+                                  : 0,
+                              0,
+                              580,
+                              340),
+                          const Size(580, 340)),
+                    ).animate(CurvedAnimation(
+                      parent: _animationController,
+                      curve: Curves.easeInOut,
+                    )),
+                    child: RotationTransition(
+                      turns: Tween<double>(
+                              begin: 0,
+                              end: swipe != Swipe.none
+                                  ? swipe == Swipe.left
+                                      ? -0.1 * 0.3
+                                      : 0.1 * 0.3
+                                  : 0.0)
+                          .animate(
+                        CurvedAnimation(
+                          parent: _animationController,
+                          curve:
+                              const Interval(0, 0.4, curve: Curves.easeInOut),
+                        ),
+                      ),
+                      child: DragWidget(
+                        profile: draggableItems[index],
+                        index: index,
+                        swipeNotifier: swipeNotifier,
+                        isLastCard: true,
+                      ),
+                    ),
+                  );
+                } else {
+                  return DragWidget(
+                    profile: draggableItems[index],
+                    index: index,
+                    swipeNotifier: swipeNotifier,
+                  );
+                }
               }),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 10,
+          left: 0,
+          right: 0,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 46.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ActionButtonWidget(
+                  onPressed: () {
+                    swipeNotifier.value = Swipe.left;
+                    _animationController.forward();
+                  },
+                  icon: const Icon(
+                    Icons.close,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(width: 20),
+                ActionButtonWidget(
+                  onPressed: () {
+                    swipeNotifier.value = Swipe.right;
+                    _animationController.forward();
+                  },
+                  icon: const Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -75,7 +165,7 @@ class _CardsStackWidgetState extends State<CardsStackWidget> {
             },
             onAccept: (int index) {
               setState(() {
-                dragabbleItems.removeAt(index);
+                draggableItems.removeAt(index);
               });
             },
           ),
@@ -98,7 +188,7 @@ class _CardsStackWidgetState extends State<CardsStackWidget> {
             },
             onAccept: (int index) {
               setState(() {
-                dragabbleItems.removeAt(index);
+                draggableItems.removeAt(index);
               });
             },
           ),
