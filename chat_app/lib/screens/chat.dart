b0833eb7b78520576cdbd1b2baa3d521f4c08a70
Paul Ruiz <ptruiz@google.com>
2024-01-20 13:13:31 -0700

    finished with chat displaying by account

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -7,24 +9,28 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter chat'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              // Don't need to pop because the stream in main.dart will handle screens
-            },
-            icon: Icon(
-              Icons.exit_to_app,
-              color: Theme.of(context).colorScheme.primary,
+        appBar: AppBar(
+          title: Text('Flutter chat'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+                // Don't need to pop because the stream in main.dart will handle screens
+              },
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary,
+              ),
             ),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text('Logged in'),
-      ),
-    );
+          ],
+        ),
+        body: const Column(
+          children: [
+            Expanded(
+              child: ChatMessages(),
+            ),
+            NewMessage(),
+          ],
+        ));
   }
 }
---
+++ chat_app/lib/widgets/new_messages.dart
@@ -0,0 +1,90 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() {
+    return _NewMessageState();
+  }
+}
+
+class _NewMessageState extends State<NewMessage> {
+  var _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void submitMessage() async {
+    final enteredMessage = _messageController.text;
+
+    // Validate
+    if (enteredMessage.trim().isEmpty) {
+      return;
+    }
+
+    // Closes keyboard
+    FocusScope.of(context).unfocus();
+
+    _messageController.clear();
+
+    // Retrieving as an example. Normally this would be stored in the app to start
+    final user = FirebaseAuth.instance.currentUser!;
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+
+    // if (userData == null) {
+    //   print("User data is nulL!");
+    //   return;
+    // }
+    // for (String key in userData!.data()!.keys) {
+    //   print(key);
+    // }
+
+    await FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 15,
+        right: 1,
+        bottom: 14,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: InputDecoration(labelText: 'send a message...'),
+            ),
+          ),
+          IconButton(
+            color: Theme.of(context).colorScheme.primary,
+            icon: const Icon(
+              Icons.send,
+            ),
+            onPressed: submitMessage,
+          )
+        ],
+      ),
+    );
+  }
+}
