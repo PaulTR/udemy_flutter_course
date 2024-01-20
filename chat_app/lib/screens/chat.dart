5bbba34560d08b69780ea562181a58b2d213ce5b
Paul Ruiz <ptruiz@google.com>
2024-01-20 13:25:42 -0700

    fcm message notifications added

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -1,36 +1,63 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() {
+    return _ChatScreenState();
+  }
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    // This is for targeting indepedent devices, but we want to target everyone
+    // final token = await fcm.getToken();
+    // print(token); // Can send to Firestore. Currently copy/pasting for testing
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter chat'),
-          actions: [
-            IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-                // Don't need to pop because the stream in main.dart will handle screens
-              },
-              icon: Icon(
-                Icons.exit_to_app,
-                color: Theme.of(context).colorScheme.primary,
-              ),
+      appBar: AppBar(
+        title: Text('Flutter chat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+              // Don't need to pop because the stream in main.dart will handle screens
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
             ),
-          ],
-        ),
-        body: const Column(
-          children: [
-            Expanded(
-              child: ChatMessages(),
-            ),
-            NewMessage(),
-          ],
-        ));
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage(),
+        ],
+      ),
+    );
   }
 }
